9705d0db6a332ab198c08149d5441995b04cffe3
Kameron Dam <kdam3@myseneca.ca>
2024-12-07 22:31:52 -0500

    commit

--- assignment2.py
+++ assignment2.py
@@ -30,6 +30,7 @@ def parse_command_args() -> object:
         "-H", "--human readable",
         action="store_strue",
         help="Prints human readable format"
+    )
     # check the docs for an argparse option to store this as a boolean.
     parser.add_argument("program", type=str, nargs='?', help="if a program is specified, show memory use of all associated processes. Show only total use is not.")
     args = parser.parse_args()
@@ -53,7 +54,7 @@ def get_sys_mem() -> int:
     with open('/proc/meminfo', 'r') as f:
         for line in f:
             if line.startswith("MemTotal"):
-                mem_total = int(line.split([1]))
+                mem_total = int(line.split()[1])
                 return mem_total
 
 
@@ -72,7 +73,7 @@ def get_avail_mem() -> int:
             elif line.startswith("SwapFree"):
                 swap_Free = int(line.split()[1])
 
-        if mem_available >= 0:
+        if mem_available is not None:
             return mem_available
         else:
             return mem_free + swap_free
@@ -80,11 +81,21 @@ def get_avail_mem() -> int:
 def pids_of_prog(app_name: str) -> list:
     "given an app name, return all pids associated with app"
     ...
-    
+    pid = []
+    result = os.open(f"pgrep {app_name}").read().strip()
+    if result:
+        pigs = result.splitlines()
+    return pids
+
 
 def rss_mem_of_pid(proc_id: str) -> int:
     "given a process id, return the resident memory used, zero if not found"
     ...
+    try:
+        with open(f"/proc/{proc_id}/stat", 'r') as f:
+            stat_info = f.read().split()
+            rss_kb = int(stat_info[23])
+        return rss_kb
 
 def bytes_to_human_r(kibibytes: int, decimal_places: int=2) -> str:
     "turn 1,024 into 1 MiB, for example"
@@ -102,20 +113,36 @@ if __name__ == "__main__":
     args = parse_command_args()
     if not args.program:
         ...
+        total_mem = get_sys_mem()
+        available_mem = get_avail_mem()
+        used_mem = total_mem - available_mem
+        percent_used = (used_mem / total_mem) * 100
+        graph = percent_to_graph(percent_used, args.length)
+
     else:
         ...
     # process args
     # if no parameter passed, 
     # open meminfo.
+    total_mem = get_sys_mem()
+    available_mem = get_avail_mem()
+
     # get used memory
+    used_mem = total_mem - available_mem
     # get total memory
     # call percent to graph
     # print
+    print(f"Total Memory: {total_mem) in kB")
+    print(f"Available Memory: {available_mem} in kB")
+    print(f"Memory Used: {used_mem} in kB")
 
     # if a parameter passed:
     # get pids from pidof
     # lookup each process id in /proc
     # read memory used
+    for pid in pids:
+        pid_mem = rss_mem_of_pid(pid)
+        total_used_mem_program += pid_mem
     # add to total used
     # percent to graph
     # take total our of total system memory? or total used memory? total used memory.
